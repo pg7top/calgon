ac1eb135e38d9b58fcbefd15d55febf685fec95b
pg7top <111295676+pg7top@users.noreply.github.com>
2023-07-05 20:56:38 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,62 +1,58 @@
 import React, { Component } from "react";
 import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
 import * as Font from "expo-font";
-import {createSwitchNavigator,createAppContainer} from "react-navigation";
+import LoginScreen from "./screens/Login";
 import BottomTabNavigator from "./components/BottomTabNavigator";
-
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
 
 export default class App extends Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    email: "",
-    password: ""
-  };
-}
-}
+  constructor() {
+    super();
+    this.state = {
+      fontLoaded: false
+    };
+  }
 
 
-handleLogin = () => {
-  const { email, password } = this.state;
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(() => {
-      this.props.navigation.navigate("BottomTab");
-    })
-    .catch(error => {
-      Alert.alert(error.message);
+  async loadFonts() {
+    await Font.loadAsync({
+      Rajdhani_600SemiBold: Rajdhani_600SemiBold
     });
-};
+    this.setState({ fontLoaded: true });
+   }
 
 
-const AppContainer = createAppContainer(AppSwitchNavigator);
+  componentDidMount() {
+    this.loadFonts();
+   }
 
-// export default class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       fontLoaded: false
-//     };
-//   }
 
-//   async loadFonts() {
-//     await Font.loadAsync({
-//       Rajdhani_600SemiBold: Rajdhani_600SemiBold
-//     });
-//     this.setState({ fontLoaded: true });
-//   }
+  render() {
+    const { fontLoaded } = this.state;
+    if (fontLoaded) {
+      return <AppContainer />;
+    }
+    return null;
+   }
+ }
 
-//   componentDidMount() {
-//     this.loadFonts();
-//   }
 
-//   render() {
-//     const { fontLoaded } = this.state;
-//     if (fontLoaded) {
-//       return <BottomTabNavigator />;
-//     }
-//     return null;
-//   }
-// }
+const AppSwitchNavigator = createSwitchNavigator(
+  {
+    Login: {
+      screen:LoginScreen
+    },
+    BottomTab: {
+      screen:BottomTabNavigator
+    }
+  },
+  {
+    initialRouteName:"Login"
+  }
+);
+
+
+ const AppContainer = createAppContainer(AppSwitchNavigator);
+
+
